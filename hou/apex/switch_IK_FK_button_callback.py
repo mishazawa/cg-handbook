94a5a5f5604b8b760528dc09ccc912d66427ab83
mishazawa <macintoshkrug@gmail.com>
2026-06-14 19:12:29 +0000

    Skip xform if module have constraints

--- hou/apex/switch_IK_FK_button_callback.py
+++ hou/apex/switch_IK_FK_button_callback.py
@@ -2,6 +2,8 @@ CONTROL_NAME = "%CONTROL_NAME%"
 SWITCH_NAME = "%SWITCH_NAME%"
 FK_CTRL_TAG = "%FK_CTRL_TAG%"
 
+from apex.scene_2 import *
+from apex.constraintutils import updateConstraintMatrices
 rigpath = state.primary_control.rpartition("/")[0]
 parms = state.scene.getData(f"{rigpath}/graph_parms")
 graph = state.scene.getData(f"{rigpath}/graph")
@@ -19,10 +21,22 @@ new_parms[f"{CONTROL_NAME}_{SWITCH_NAME}"] = not curr_blend
 
 new_mapping = apex.Dict()
 
-graph_parms = apex.Dict()
-graph_parms[rigpath] = new_parms
-
 ctrls_path = [state.primary_control, f"{rigpath}/{CONTROL_NAME}"]
+
+
+# hardcoded, but kind of not configurable from outside
+root_ctrl = graph.matchNodes("%tag(module_root)")
+if len(root_ctrl):
+    root_ctrl = graph.nodeName(root_ctrl[0])
+else:
+    root_ctrl = "-1"
+
+ik_root = graph.matchNodes("%tag(ik_root)")
+if len(ik_root):
+    ik_root = graph.nodeName(ik_root[0])
+else:
+    ik_root = "-1"
+
 
 
 # create mapping
@@ -63,13 +77,44 @@ for nodeid in bind_joints:
                     new_parms[key] = hou.Vector3()
             ctrls_path.append(f"{rigpath}/{ikjnt}")
 
+# check if module constrained in animation state
+# and not set xform to ik root when switching
+def patch_xform_if_ik_constrained(xform, tgt):
+    # ignore for FK
+    if next_state != "IK":
+        return xform
+
+    # if no IK joints found
+    if root_ctrl == "-1" or ik_root == "-1":
+        return xform
+
+    # if target not ik root
+    if tgt != ik_root:
+        return xform
+
+    constr_ctrl = getConstrainedControls(state.scene)
+    # if not constrained
+    if not len(constr_ctrl):
+        return xform
+
+    for ctrl in constr_ctrl:
+        _, _, b = ctrl.rpartition("/")
+        if b == root_ctrl:
+            return None # Found!
+    return xform
+
+graph_parms = apex.Dict()
+graph_parms[rigpath] = new_parms
+
 for src, tgt in mapping.items():
     xform = state.scene.getGraphOutputData(
         f"{rigpath}/control_data", key=f"{src}__xform"
     )
     if xform:
-        new_mapping[f"{rigpath}/{tgt}"] = xform
-
+        xform = patch_xform_if_ik_constrained(xform, tgt)
+        if xform:
+            new_mapping[f"{rigpath}/{tgt}"] = xform
+    
 
 with hou.undos.group("my_callback"):
     state.updateControls(graph_parms, ctrls_path)
